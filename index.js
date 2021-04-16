c787aecb93d18c331a799e24a03162b64ebf9beb
Oroko <aoroko09@gmail.com>
2021-04-16 10:05:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+const submitData = (name, email) => {
+  const postData = {
+    name: name,
+    email: email,
+  };
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(postData),
+  };
+  return fetch("http://localhost:3000/users", configObj)
+    .then((res) => res.json())
+    .then((data) => {
+      document.body.innerHTML = `${data.id}`;
+    })
+    .catch((error) => {
+      document.body.innerHTML = `${error.message}`;
+    });
+};
